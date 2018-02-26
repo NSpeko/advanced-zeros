b4d09a6c0f5d130a796c8a4293d5286d56331299
NSpeko <nonstoper42@gmail.com>
2018-02-26 16:26:46 +0300

    Feature: Made some terruble calculations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+    let zeros = Infinity;
+    function downGrade(number, primeNumber) {
+        return number<primeNumber?0:Math.floor(number/primeNumber)+downGrade(Math.floor(number/primeNumber),primeNumber);
+    }
+    let primeNumbers = {};
+    for(let i=2; i*i<=base; i++) {
+        if(base%i===0){
+            while(base%i===0){
+                base/=i;
+            }
+        }
+    }
+    if(base>1){
+        primeNumbers[base]=1;
+    }
+    for(let prime in primeNumbers) zeros = Math.min(zeros,Math.floor(downGrade(number,prime)/primeNumbers[prime]))
+    return zeros;
 }
