fb830d02ead20f3f3ae1cc329f090e7f38a666bd
NSpeko <nonstoper42@gmail.com>
2018-02-28 10:45:24 +0300

    Fix: Corrected task

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ module.exports = function getZerosCount(number, base) {
     for(let i=2; i*i<=base; i++) {
         if(base%i===0){
             while(base%i===0){
+                primeNumbers[i]=primeNumbers[i]?primeNumbers[i]+1:1;
                 base/=i;
             }
         }
     }
     if(base>1){
-        primeNumbers[base]=1;
+        primeNumbers[base]=primeNumbers[base]?primeNumbers[base]+1:1;
     }
     for(let prime in primeNumbers) zeros = Math.min(zeros,Math.floor(downGrade(number,prime)/primeNumbers[prime]))
     return zeros;
